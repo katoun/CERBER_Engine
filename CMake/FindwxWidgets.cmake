af7d87367c14997bf165e9ba8622a5e1d6bc3823
Catalin Alexandru Nastase <katoun9@gmail.com>
2013-08-24 17:25:02 +0300

    small updates to wxWidgets linking.

--- CMake/FindwxWidgets.cmake
+++ CMake/FindwxWidgets.cmake
@@ -35,6 +35,59 @@ find_path(wxWidgets_INCLUDE_DIR
 	PATHS ${WXWIDGETS_SEARCH_PATHS}
 )
 
+set(wxWidgets_LIBRARIES "")
+
+foreach(LIB zlib png tiff jpeg expat regex)
+    find_library(WX_${LIB}
+		NAMES wx${LIB}
+		HINTS
+		NO_DEFAULT_PATH
+		NO_CMAKE_ENVIRONMENT_PATH
+		NO_CMAKE_SYSTEM_PATH
+		NO_SYSTEM_ENVIRONMENT_PATH
+		NO_CMAKE_PATH
+		PATH_SUFFIXES lib lib64 win32/Dynamic_Release "Win32/${MSVC_YEAR_NAME}/x64/Debug" "Win32/${MSVC_YEAR_NAME}/Win32/Debug"
+		PATHS ${WXWIDGETS_SEARCH_PATHS}
+	)
+	
+	# First search for d-suffixed libs
+	find_library(WX_${LIB}_DEBUG 
+		NAMES wx${LIB}d
+		HINTS
+		NO_DEFAULT_PATH
+		NO_CMAKE_ENVIRONMENT_PATH
+		NO_CMAKE_SYSTEM_PATH
+		NO_SYSTEM_ENVIRONMENT_PATH
+		NO_CMAKE_PATH
+		PATH_SUFFIXES lib lib64 win32/Dynamic_Debug "Win32/${MSVC_YEAR_NAME}/x64/Debug" "Win32/${MSVC_YEAR_NAME}/Win32/Debug"
+		PATHS ${WXWIDGETS_SEARCH_PATHS}
+	)
+	
+	if(NOT WX_${LIB}_DEBUG)
+		# Then search for non suffixed libs if necessary, but only in debug dirs
+		find_library(WX_${LIB}_DEBUG
+			NAMES wx${LIB}
+			HINTS
+			NO_DEFAULT_PATH
+			NO_CMAKE_ENVIRONMENT_PATH
+			NO_CMAKE_SYSTEM_PATH
+			NO_SYSTEM_ENVIRONMENT_PATH
+			NO_CMAKE_PATH
+			PATH_SUFFIXES win32/Dynamic_Debug "Win32/${MSVC_YEAR_NAME}/x64/Debug" "Win32/${MSVC_YEAR_NAME}/Win32/Debug"
+			PATHS ${WXWIDGETS_SEARCH_PATHS}
+		)
+	endif()
+	
+	if(WX_${LIB})
+		if(WX_${LIB}_DEBUG)
+			list(APPEND wxWidgets_LIBRARIES optimized "${WX_${LIB}}" debug "${WX_${LIB}_DEBUG}")
+		else()
+			list(APPEND wxWidgets_LIBRARIES "${WX_${LIB}}")		# Could add "general" keyword, but it is optional
+	endif()
+endif()
+	
+endforeach()
+
 find_library(WX_base 
 	NAMES wxbase29u
 	HINTS
@@ -47,7 +100,41 @@ find_library(WX_base
 	PATHS ${WXWIDGETS_SEARCH_PATHS}
 )
 
-set(wxWidgets_LIBRARIES "")
+# First search for d-suffixed libs
+find_library(WX_base_DEBUG 
+	NAMES wxbase29ud
+	HINTS
+	NO_DEFAULT_PATH
+	NO_CMAKE_ENVIRONMENT_PATH
+	NO_CMAKE_SYSTEM_PATH
+	NO_SYSTEM_ENVIRONMENT_PATH
+	NO_CMAKE_PATH
+	PATH_SUFFIXES lib lib64 win32/Dynamic_Debug "Win32/${MSVC_YEAR_NAME}/x64/Debug" "Win32/${MSVC_YEAR_NAME}/Win32/Debug"
+	PATHS ${WXWIDGETS_SEARCH_PATHS}
+)
+	
+if(NOT WX_base_DEBUG)
+	# Then search for non suffixed libs if necessary, but only in debug dirs
+	find_library(WX_base_DEBUG
+		NAMES wxbase29ud
+		HINTS
+		NO_DEFAULT_PATH
+		NO_CMAKE_ENVIRONMENT_PATH
+		NO_CMAKE_SYSTEM_PATH
+		NO_SYSTEM_ENVIRONMENT_PATH
+		NO_CMAKE_PATH
+		PATH_SUFFIXES win32/Dynamic_Debug "Win32/${MSVC_YEAR_NAME}/x64/Debug" "Win32/${MSVC_YEAR_NAME}/Win32/Debug"
+		PATHS ${WXWIDGETS_SEARCH_PATHS}
+	)
+endif()
+
+if(WX_base)
+	if(WX_base_DEBUG)
+		list(APPEND wxWidgets_LIBRARIES optimized "${WX_base}" debug "${WX_base_DEBUG}")
+	else()
+		list(APPEND wxWidgets_LIBRARIES "${WX_base}")		# Could add "general" keyword, but it is optional
+	endif()
+endif()
 
 foreach(LIB core adv aui propgrid)
     find_library(WX_${LIB}
@@ -99,6 +186,11 @@ foreach(LIB core adv aui propgrid)
 endif()
 	
 endforeach()
+
+## add system libraries wxWidgets always seems to depend on
+if(WIN32)
+	list(APPEND wxWidgets_LIBRARIES winmm comctl32 rpcrt4 wsock32)
+endif()
 
 # handle the QUIETLY and REQUIRED arguments and set XXX_FOUND to TRUE if all listed variables are TRUE
 include(FindPackageHandleStandardArgs)
--- build_dependencies_vc11.bat
+++ build_dependencies_vc11.bat
@@ -1,12 +1,13 @@
 SET VisualStudioVersion=11.0
 
+cd Dependencies
 rmdir /s /q Build
 mkdir Build
 cd Build
-
 cmake -G "Visual Studio 11" ..
 
 "%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" /nologo /property:Configuration=Debug ALL_BUILD.vcxproj
 "%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" /nologo /property:Configuration=Release ALL_BUILD.vcxproj
 
+cd..
 cd..
